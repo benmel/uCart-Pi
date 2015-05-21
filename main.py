f0dcb70f67c7d1ff73f5f927e1793883ae005cc8
Ben Meline <benmeline@gmail.com>
2015-05-21 11:18:08 -0500

    Application

---
+++ main.py
@@ -0,0 +1,38 @@
+from bluetooth import *
+import logging
+
+logging.basicConfig(filename="rfcomm.log", level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+try:
+  while True:                   
+    logger.info("Waiting for connection on RFCOMM channel %d", port)
+    client_sock, client_info = server_sock.accept()
+    logger.info("Accepted connection from %s", client_info)
+
+    try:
+      while True:
+        try:
+          text = raw_input()
+          logger.info("Sending text: %s", text)
+          text += '\n'
+          client_sock.send(text)
+        except IOError:
+          logger.error("IOError")
+          pass
+    except KeyboardInterrupt:    
+      logger.info("Disconnected")
+      client_sock.close()
+      server_sock.close()
+      break
+
+except KeyboardInterrupt:
+  pass
+
+logger.info("Exiting")
